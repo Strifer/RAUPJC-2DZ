03fba5665de07a1a90b740e374f2287c4df91408
fdzidic <filipdzidic@gmail.com>
2016-10-30 02:23:23 +0200

    Answered all README.md questions. Finished Zadatak6.

--- RAUPJC-2DZ/Zadatak6/Program.cs
+++ RAUPJC-2DZ/Zadatak6/Program.cs
@@ -10,17 +10,58 @@ namespace Zadatak6
 {
     public class Program
     {
+
+        private static object _lock = new object(); 
+
         public static void Main(string[] args)
         {
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            LongOperation("A");
-            LongOperation("B");
-            LongOperation("C");
-            LongOperation("D");
-            LongOperation("E");
+            //LongOperation("A");
+            //LongOperation("B");
+            //LongOperation("C");
+            //LongOperation("D");
+            //LongOperation("E");
             stopwatch.Stop();
-            Console.WriteLine(" Synchronous long operation calls finished {0} sec.", stopwatch.Elapsed.TotalSeconds);            Console.ReadLine();
+            Console.WriteLine(" Synchronous long operation calls finished {0} sec.", stopwatch.Elapsed.TotalSeconds);            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.Invoke(() => LongOperation("A"),
+                            () => LongOperation("B"),
+                            () => LongOperation("C"),
+                            () => LongOperation("D"),
+                            () => LongOperation("E"));
+            stopwatch.Stop();
+            Console.WriteLine(" Parallel long operation calls finished {0} sec.", stopwatch.Elapsed.TotalSeconds);            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.For(0, 1000, (i) =>
+            {
+                var x = 2;
+                var y = 2;
+                var sum = x + y;
+            });
+            stopwatch.Stop();
+
+            Console.WriteLine(" Parallel calls finished {0} ms.", stopwatch.Elapsed.TotalMilliseconds);
+            stopwatch.Restart();
+            for (int i = 0; i < 1000; i++)
+            {
+                int x = 2;
+                int y = 2;
+                int sum = x + y;
+            }
+            stopwatch.Stop();
+            Console.WriteLine(" Sync operation calls finished {0} ms.", stopwatch.Elapsed.TotalMilliseconds);            //ZADATAK 6            List<int> results = new List<int>();
+           
+            Parallel.For(0, 100, (i) =>
+            {
+                Thread.Sleep(1);
+                lock(_lock)
+                {
+                    results.Add(i * i);
+                }
+                
+            });
+            Console.WriteLine("Bag length should be 100. Length is {0}", results.Count);            Console.ReadLine();
         }
 
         public static void LongOperation(string taskName)
--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Izvođenje je trajalo 5.003792 sekundi.
 
 #Pitanje 2:
 Operacije su se izvodile slijedno na jednoj dretvi.
+
+#Pitanje 3:
+Izvođenje je trajalo 1.1213161 sekundi.
+
+#Pitanje 4:
+Operacije su se izvodile na pet dretvi.
+
+#Pitanje 5:
+Paralelna petlja je trajala 0.328 milisekundi. Slijedna petlja je trajala 0.0035 milisekundi.
